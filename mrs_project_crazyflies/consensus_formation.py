18e2c66417c16a70c5b2c62471d488e8fc0bd757
Renatto Tommasi <renatto.tommasi@gmail.com>
2025-01-13 16:49:19 +0100

    Skeleton for formation

--- mrs_project_crazyflies/consensus_formation.py
+++ mrs_project_crazyflies/consensus_formation.py
@@ -7,15 +7,60 @@ class ConsensusFormationController(Node):
 
         self.get_logger().info(f"Consensus Formation Controller started correctly!")
 
+        self.num_of_robots = 4 # Manual for now
+
+    def set_formation(self, formation):
+        """
+        Description: This function stablishes the formation to be obtained
+        Input: 
+            formation: string -> triangle, square, ... etc
+
+        Output:
+            xi: nd.array (n,2) -> positions vector
+        """
+        #TODO: Build the position vector for each of the agents
+        pass
+
+    def get_locations(self):
+        """
+        Description: This function forms a vector with the location of all the boids
+
+        Output:
+            X: nd.array (n,2) -> locations vector
+        """
+        #TODO: Build the locations vector with all the agents from a subsriber to the position topic
+        pass
+
+    def calculate_formation_vel(self, X, xi):
+        """
+        Description: This function calculate the velocities to achieve the formation for each robot
+        Input: 
+            X: nd.array (n,2) -> locations vector
+            xi: nd.array (n,2) -> positions vector
+
+        Output:
+            V: nd.array (n,2) -> velocities vector
+        """
+        #TODO: Calculate the velocities for each agent using the equation in the slides
+        pass
+
+    def send_velocities(self, V):
+        """
+        Description: This function publishes the velocity to each boid
+        Input: 
+            V: nd.array (n,2) -> velocities vector
+
+        """
+        #TODO: use a publisher to access all boid velocities
+        pass
+
+
 
 def main(args=None):
     rclpy.init(args=args)
     
-
-
     # Instantiate the Consensus Controller
     controller = ConsensusFormationController()
-    # print(f"Node {robot_id} / {num_of_robots} started correctly!")
 
     # Keep the node alive until manually interrupted
     rclpy.spin(controller)

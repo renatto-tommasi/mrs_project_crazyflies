c470285a3b588193b558293d63cb5a622d4c9366
Renatto Tommasi <renatto.tommasi@gmail.com>
2025-01-14 18:04:31 +0100

    Added launching sequence

--- mrs_project_crazyflies/consensus_rendezvous.py
+++ mrs_project_crazyflies/consensus_rendezvous.py
@@ -2,6 +2,9 @@ import rclpy
 from rclpy.node import Node
 
 from geometry_msgs.msg import Pose, Twist, PoseStamped, Point
+from nav_msgs.msg import Odometry
+
+import numpy as np
 
 class ConsensusRendezvousController(Node):
     def __init__(self):
@@ -9,17 +12,35 @@ class ConsensusRendezvousController(Node):
 
         self.get_logger().info(f"Consensus Rendezvous Controller started correctly!")
         num_of_robots = 4
+        timer_period = 3  # secondstimer_period = 0.5  # seconds
+        self.takeoff = 0
+
+        self.vel = None
 
         # PUBLISHERS
-        self.vel_publishers = [self.create_publisher(Twist, f"/cr_{i}/cmd_vel", 10) for i in range(num_of_robots)]
+        self.vel_publishers = [self.create_publisher(Twist, f"/cf_{i+1}/cmd_vel", 10) for i in range(num_of_robots)]
 
         # SUBSCRIBERS
+        self.odom_subscribers = [self.create_subscription(Odometry, f"/cf_{i+1}/odom",self.store_odom, 10) for i in range(num_of_robots)]
 
-        self.send_velocities()
+        # # TIMERS
+        # self.timer = self.create_timer(timer_period, self.update_vel)
+
+
 
 
         self.num_of_robots = 4 # Manual for now
 
+    def update_vel(self, drone_id, vel):
+        pass
+        
+
+    def store_odom(self, msg:Odometry):
+        x = msg.pose.pose.position.x
+        y = msg.pose.pose.position.y
+        z = msg.pose.pose.position.z
+        frame = msg.child_frame_id
+        self.get_logger().info(f"Drone {frame} is at x:{x}, y:{y}, z{z}")
     
 
     def set_formation(self, formation):
@@ -57,17 +78,24 @@ class ConsensusRendezvousController(Node):
         #TODO: Calculate the velocities for each agent using the equation in the slides
         pass
 
-    def send_velocities(self, linear_x=0.5, angular_z=0.0):
+    def launch_drones(self):
         """
         Description: This function publishes the velocity to each boid
         Input: 
             V: nd.array (n,2) -> velocities vector
 
         """
+        
+        
+        linear_z = float(0.5)
+
+
         # Create the Twist message
         twist_msg = Twist()
-        twist_msg.linear.x = linear_x
-        twist_msg.angular.z = angular_z
+        twist_msg.linear.z = linear_z
+
+        self.get_logger().info(f"Launching Drones")
+        
 
         # Publish the same message to all publishers
         for publisher in self.vel_publishers:
@@ -80,6 +108,7 @@ def main(args=None):
     
     # Instantiate the Consensus Controller
     controller = ConsensusRendezvousController()
+    controller.launch_drones()
 
     # Keep the node alive until manually interrupted
     rclpy.spin(controller)

9b4ca1da52e5a6edfe77f6d5ad86bb3d259e43a9
Renatto Tommasi <renatto.tommasi@gmail.com>
2025-01-30 12:58:12 +0100

    added num_of_robots to reynolds launch file

--- launch/reynolds.launch.py
+++ launch/reynolds.launch.py
@@ -1,17 +1,18 @@
 # reynolds.launch.py
 
-import os
-from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration, TextSubstitution
+from launch_ros.actions import Node
 
 def generate_launch_description():
     # Command-line argument for number of robots
     num_of_robots_launch_arg = DeclareLaunchArgument(
         "num_of_robots", default_value=TextSubstitution(text="3")
     )
+
+    # Get the number of robots from the launch configuration
+    num_of_robots = LaunchConfiguration('num_of_robots')
 
     # Create multiple boid nodes
     boid_nodes = [
@@ -21,10 +22,10 @@ def generate_launch_description():
             executable='reynolds',
             name='reynolds_sim',
             parameters=[{
-                "num_of_robots": LaunchConfiguration('num_of_robots'),
+                "num_of_robots": num_of_robots, 
                 "robot_id": i+1,
             }]
-        ) for i in range(3)
+        ) for i in range(int(num_of_robots.perform(None)))
     ]
 
     return LaunchDescription([num_of_robots_launch_arg] + boid_nodes)
